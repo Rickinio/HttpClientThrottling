2b32f6119b94d8eabed90871f8bbbff10d33ffcc
Konstantinos Stamatopoulos <kostamat@microsoft.com>
2020-04-09 00:55:48 +0300

    Add project files.

---
+++ HttpClientThrottling/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpClientThrottling
+{
+    class Program
+    {
+        private static HttpClient httpClient = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            var tasks = new List<Task>();
+            // here we set the max allowed concurrent request
+            var throttler = new SemaphoreSlim(3);
+
+            for (var i = 0; i < 12; i++)
+            {
+                // let's wait here until we can pass from the Semaphore
+                await throttler.WaitAsync();
+
+                // add our task logig here
+                tasks.Add(Task.Run(async () =>
+                {
+                    try
+                    {
+                        var result = await ExecuteRequest();
+                        
+                        // let's wait here for 1 second here to honor the API's rate limit
+                        await Task.Delay(1000);
+                    }
+                    finally
+                    {
+                        throttler.Release();
+                    }
+                }));
+            }
+
+            // await for all the tasks to complete
+            await Task.WhenAll(tasks.ToArray());
+        }
+
+        private static async Task<HttpResponseMessage> ExecuteRequest()
+        {
+            var result = await httpClient.GetAsync("https://www.bing.com");
+            return result;
+        }
+    }
+}
